c95334eefd7eb723c454ae7dcad905ba53522b30
jabir254 <jabirkullow@gmail.com>
2023-11-08 19:59:42 +0300

    task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request, Flask
 from typing import List, TypeVar
 
+
 class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """_summary_
@@ -17,11 +18,11 @@ class Auth():
         """
         if path is None:
             return False
-    
-    def authorization_header(self, request = None) -> str:
+
+    def authorization_header(self, request=None) -> str:
         if request is None:
             return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         if request is None:
             return None
