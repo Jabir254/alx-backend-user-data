e7fd91d03d03422eba4fbd3c87732beaf5638637
jabir254 <jabirkullow@gmail.com>
2023-11-14 20:20:11 +0300

    task 2 find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -43,3 +45,22 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_usr_by(self, **kwargs) -> User:
+        """find users and return the first row
+
+        Raises:
+            InvalidRequestError: when no result if found
+            NoResultFound: wrong query arg are passed
+
+        Returns:
+            User: filtered table by methods 
+        """
+        users = self._session.query(User)
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise InvalidRequestError
+            for usr in users:
+                if getattr(usr, k) == v:
+                    return usr
+        raise NoResultFound
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,13 +9,11 @@ Base = declarative_base()
 
 
 class User(Base):
+    '''Defination of class User'''
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        return "<User(email='%s')>" % (
-            self.email)
